b9286884dc092f0e32960839921b70c632c93611
breezybrow <browder.bree@gmail.com>
2022-03-15 21:05:40 -0500

    Resolve git issues

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,3 +1,4 @@
+import { getFullYear, getFooterCopy } from './utils';
 import logo from './Holbie-logo.jpg';
 import './App.css';
 
@@ -12,7 +13,7 @@ function App() {
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
       </div>
     </div>
   );
