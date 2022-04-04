185af63a2855b6337badd6e4e2638a82be5e080e
breezybrow <browder.bree@gmail.com>
2022-04-04 13:16:38 -0500

    chained mutations

---
+++ 0x01-React_Immutable/4-mutations.js
@@ -0,0 +1,16 @@
+import { List, Map } from 'immutable';
+
+const map = Map({
+    1: 'Liam',
+    2: 'Noah',
+    3: 'Elijah',
+    4: 'Oliver',
+    5: 'Jacob',
+    6: 'Lucas',
+});
+
+const map2 = List(map).withMutations((list) => {
+    list.set(2, 'Benjamin').set(4, 'Oliver');
+});
+
+export { map, map2 };
