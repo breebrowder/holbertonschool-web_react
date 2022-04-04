3089b3de9a45d7c4eb362a404814744ec8429d08
breezybrow <browder.bree@gmail.com>
2022-04-04 13:22:43 -0500

    5-merge

--- 0x01-React_Immutable/5-merge.js
+++ 0x01-React_Immutable/5-merge.js
@@ -1,17 +1,17 @@
 import { List, Map } from 'immutable';
 
 function concatElements(page1, page2) {
-    const p1 = List(page1);
-    const p2 = List(page2);
-    const pList = p1.concat(p2);
-    return (pList);
+  const p1 = List(page1);
+  const p2 = List(page2);
+  const pList = p1.concat(p2);
+  return (pList);
 }
 
 function mergeElements(page1, page2) {
-    const p1 = Map(page1);
-    const p2 = Map(page2);
-    const pList = p1.merge(p2);
-    return (pList);
+  const p1 = Map(page1);
+  const p2 = Map(page2);
+  const pList = p1.merge(p2);
+  return (pList);
 }
 
 module.exports = { concatElements, mergeElements };
