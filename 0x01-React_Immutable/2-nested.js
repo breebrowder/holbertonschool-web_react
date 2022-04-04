482b5bc8e2db278f61bfd17380c21cafe5c90787
breezybrow <browder.bree@gmail.com>
2022-04-04 12:16:15 -0500

    satisfy linter

--- 0x01-React_Immutable/2-nested.js
+++ 0x01-React_Immutable/2-nested.js
@@ -1,5 +1,5 @@
 import { getIn, fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-    return getIn(fromJS(object), array);
+  return getIn(fromJS(object), array);
 }
