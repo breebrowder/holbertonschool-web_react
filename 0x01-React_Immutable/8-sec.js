eac7a2e016842ea14f81508d923d9602b6d426ef
breezybrow <browder.bree@gmail.com>
2022-04-04 19:11:57 -0500

    8-sec

---
+++ 0x01-React_Immutable/8-sec.js
@@ -0,0 +1,14 @@
+import { sec } from 'immutable';
+
+function printBestStudents(obj) {
+    const studentSeq = Seq(obj)
+        .filter((x) => x.score >= 70)
+        .map((x) => ({
+            score: x.score,
+            firstName: x.firstName.charAt(0).toUpperCase() + x.firstName.slice(1),
+            lastName: x.lastName.charAt(0).toUpperCase() + x.lastName.slice(1),
+        }));
+    console.log(studentSeq.toObject());
+}
+
+module.exports = printBestStudents;
